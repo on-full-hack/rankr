1f685d9c1411d5b3b0ca0684c3c1a5162e7161a7
Filip Czabator <filip.czabator@vazco.eu>
2019-12-27 23:16:17 +0100

    Added auth0 support on backend

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,30 +1,17 @@
-import express, { Application } from 'express';
-import { Request, Response, NextFunction } from 'express-serve-static-core';
+import express from 'express';
 import config from './config';
 import { startControllers } from './startControllers';
-import bodyParser from 'body-parser';
-import mongoose from 'mongoose';
+import { applyMiddlewares } from './middlewares';
 
 startup();
 
 async function startup() {
   const app = express();
   const router = express.Router();
-  applyMiddleware(app);
+  applyMiddlewares(app);
   startControllers(router);
   app.use('/api', router);
 
   console.log(`🚀 Server ready at port ${config.server.port}`);
   app.listen(config.server.port);
-}
-
-function applyMiddleware(app: Application) {
-  app.use(logRequest);
-  app.use(bodyParser.urlencoded({ extended: true }));
-  app.use(bodyParser.json());
-}
-
-function logRequest(req: Request, res: Response, next: NextFunction) {
-  console.info(`${req.method} ${req.originalUrl}`);
-  next();
 }
