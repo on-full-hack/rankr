ece72f8a7377281d80136102c5402982780b9899
Lukasz <lukaszwypchal@gmail.com>
2020-02-29 16:52:53 +0100

    delete league by creator

--- server/src/controllers/LeagueController.ts
+++ server/src/controllers/LeagueController.ts
@@ -20,7 +20,14 @@ export default class LeagueController {
       discipline,
       type
     );
-    sendResponse[result](res);
+    sendResponse[result.status](res, result.league);
+  };
+
+  deleteLeague = async (req: Request, res: Response) => {
+    const league_id = req.params.id;
+    const { user_id } = req.headers;
+    const result = await this.leagueService.deleteLeague(user_id.toString(), league_id);
+    sendResponse[result.status](res, result.league);
   };
 }
 
@@ -29,8 +36,12 @@ const sendResponse = {
     res.status(501);
     res.send(JSON.stringify({ message: Constants.ERROR }));
   },
-  [Constants.LEAGUE_CREATED]: (res: Response) => {
+  [Constants.LEAGUE_CREATED]: (res: Response, league: Object) => {
     res.status(201);
-    res.send(JSON.stringify({ message: Constants.LEAGUE_CREATED }));
+    res.send(JSON.stringify({ message: Constants.LEAGUE_CREATED, league: league }));
+  },
+  [Constants.LEAGUE_DELETED]: (res: Response, league: Object) => {
+    res.status(200);
+    res.send(JSON.stringify({ message: Constants.LEAGUE_DELETED, league: league }));
   },
 };
--- server/src/services/LeagueService.ts
+++ server/src/services/LeagueService.ts
@@ -1,5 +1,5 @@
 import LeagueRepository from '../persistance/repositories/LeagueRepository';
-import Consants from '../controllers/Constants';
+import Constants from '../controllers/Constants';
 
 export default class LeagueService {
   constructor(public leagueRepository: LeagueRepository) { }
@@ -14,12 +14,29 @@ export default class LeagueService {
 
   addLeague = async (creator_id: string, name: string, description: string, discipline: string, type: string) => {
     try {
-      await this.leagueRepository.addLeague(creator_id, name, description, discipline, type);
-      return Consants.LEAGUE_CREATED;
+      const league = await this.leagueRepository.addLeague(creator_id, name, description, discipline, type);
+      console.log(league);
+      return { status: Constants.LEAGUE_CREATED, league: league };
     }
     catch (e) {
       console.error(e);
-      return Consants.ERROR;
+      return { status: Constants.ERROR, league: {} };
     }
   };
+
+  deleteLeague = async (creator_id: string, league_id: string) => {
+    try {
+      const league = await this.leagueRepository.findById(league_id);
+      if (league.creator_id === creator_id) {
+        await this.leagueRepository.removeOne({ _id: league_id });
+        return { status: Constants.LEAGUE_DELETED, league: league };
+      } else {
+        console.error("Provided user is not a creator");
+        return { status: Constants.ERROR, league: {} };
+      }
+    } catch (e) {
+      console.error(e);
+      return { status: Constants.ERROR, league: {} };
+    }
+  }
 }
