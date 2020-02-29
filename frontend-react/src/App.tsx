abf22b93cad1173ab0de98d49a7f2458ec6ed807
Filip Czabator <filipczabator@Filips-MacBook-Pro.local>
2020-02-29 11:14:04 +0100

    Added routing

--- frontend-react/src/App.tsx
+++ frontend-react/src/App.tsx
@@ -1,14 +1,27 @@
 import React from 'react';
 import { theme } from './lib/theme';
 import { ThemeProvider } from '@material-ui/core';
+import { BrowserRouter, Route } from 'react-router-dom';
 import { AppBar } from './components/AppBar';
+import { Home } from './components/Home';
+import { Leagues } from './components/Leagues';
 
 export const App = () => {
   return (
     <ThemeProvider theme={theme}>
-      <AppBar />
+      <BrowserRouter>
+        <Route path="/">
+          <AppBar />
+        </Route>
+        <Route path="/" exact>
+          <Home />
+        </Route>
+        <Route path="/leagues">
+          <Leagues />
+        </Route>
+      </BrowserRouter>
     </ThemeProvider>
-  )
-}
+  );
+};
 
 export default App;
